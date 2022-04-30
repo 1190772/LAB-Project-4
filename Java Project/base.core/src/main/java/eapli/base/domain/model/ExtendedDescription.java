be372e51a6f51ccbf51644fe6ff8daa4397aaab5
1190384 <1190384@isep.ipp.pt>
2022-04-30 01:38:49 +0100

    #3 Short/Extended/TechnicalDescription Classes

---
+++ Java Project/base.core/src/main/java/eapli/base/domain/model/ExtendedDescription.java
@@ -0,0 +1,55 @@
+package eapli.base.domain.model;
+
+import eapli.framework.domain.model.ValueObject;
+import eapli.framework.validations.Preconditions;
+
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class ExtendedDescription implements ValueObject, Serializable, Comparable<ExtendedDescription> {
+
+    private String extDescription;
+
+    public ExtendedDescription(String extDescription) {
+        Preconditions.nonNull(extDescription);
+        if (!validateExtendedDescription(extDescription))
+            throw new IllegalArgumentException("invalid extended description lehgth");
+        this.extDescription = extDescription;
+    }
+
+    protected ExtendedDescription() {
+    }
+
+    private static boolean validateExtendedDescription(String extDescription) {
+        return extDescription.length() > Constantes.MIN_EXTHDESC_LENGTH && extDescription.length() < Constantes.MAX_EXTDESC_LENGTH;
+    }
+
+    public static ExtendedDescription extendedDescriptedAs(String extendedDescription) {
+        return new ExtendedDescription(extendedDescription);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ExtendedDescription that = (ExtendedDescription) o;
+        return Objects.equals(extDescription, that.extDescription);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(extDescription);
+    }
+
+    @Override
+    public int compareTo(ExtendedDescription o) {
+        return this.extDescription.compareTo(o.extDescription);
+    }
+
+    @Override
+    public String toString() {
+        return this.extDescription;
+    }
+}
---
+++ Java Project/base.core/src/main/java/eapli/base/domain/model/TechnicalDescription.java
@@ -0,0 +1,56 @@
+package eapli.base.domain.model;
+
+import eapli.framework.domain.model.ValueObject;
+import eapli.framework.validations.Preconditions;
+
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class TechnicalDescription implements ValueObject, Serializable, Comparable<TechnicalDescription> {
+
+    private String tecDescription;
+
+    public TechnicalDescription(String tecDescription) {
+        Preconditions.nonNull(tecDescription);
+        if (!validateTechnicalDescription(tecDescription))
+            throw new IllegalArgumentException("invalid thecnical description lehgth");
+        this.tecDescription = tecDescription;
+    }
+
+    protected TechnicalDescription() {
+        this.tecDescription = null;
+    }
+
+    private static boolean validateTechnicalDescription(String tecDescription) {
+        return tecDescription.length() > Constantes.MIN_TECHDESC_LENGTH && tecDescription.length() < Constantes.MAX_TECHDESC_LENGTH;
+    }
+
+    public static TechnicalDescription tehcnincalDescriptedAs(String technicalDeescription) {
+        return new TechnicalDescription(technicalDeescription);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TechnicalDescription that = (TechnicalDescription) o;
+        return Objects.equals(tecDescription, that.tecDescription);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(tecDescription);
+    }
+
+    @Override
+    public int compareTo(TechnicalDescription o) {
+        return this.tecDescription.compareTo(o.tecDescription);
+    }
+
+    @Override
+    public String toString() {
+        return this.tecDescription;
+    }
+}
