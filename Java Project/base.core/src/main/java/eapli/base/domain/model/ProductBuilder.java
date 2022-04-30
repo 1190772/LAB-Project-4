ec4463643c839e3eb9693011712575324313b321
1190384 <1190384@isep.ipp.pt>
2022-04-30 01:31:53 +0100

    #3 ProductBuilder classe

--- Java Project/base.core/src/main/java/eapli/base/domain/model/ProductBuilder.java
+++ Java Project/base.core/src/main/java/eapli/base/domain/model/ProductBuilder.java
@@ -1,2 +1,141 @@
-package eapli.base.domain.model;public class ProductBuilder {
+package eapli.base.domain.model;
+
+import eapli.framework.domain.model.DomainFactory;
+
+import java.util.Set;
+
+public class ProductBuilder implements DomainFactory<Product> {
+
+    private Product product;
+
+    private Category category;
+
+    private InternalCode internalCode;
+
+    private ShortDescription shortDescription;
+
+    private ExtendedDescription extendedDescription;
+
+    private TechnicalDescription technicalDeescription;
+
+    private Price price;
+
+    private Reference reference;
+
+    private Brand brand;
+
+    private ProductionCode productionCode;
+
+    private Barcode barcode;
+
+    public ProductBuilder ofType(final Category categoriaProduto) {
+        category = categoriaProduto;
+        return this;
+    }
+
+    public ProductBuilder internalCoded(final InternalCode code) {
+        this.internalCode = code;
+        return this;
+    }
+
+    public ProductBuilder shortDesignated(final ShortDescription shortDescription) {
+        this.shortDescription = shortDescription;
+        return this;
+    }
+
+
+    public ProductBuilder extendedDesignated(final ExtendedDescription extendedDescription) {
+        this.extendedDescription = extendedDescription;
+        return this;
+    }
+
+
+    public ProductBuilder barcoded(final Barcode barcode) {
+        this.barcode = barcode;
+        return this;
+    }
+
+
+    public ProductBuilder branded(final Brand brand) {
+        buildOrThrow();
+        product.changeBrandTo(brand);
+        return this;
+    }
+
+    public ProductBuilder technicalDesignated(final TechnicalDescription technicalDescription) {
+        buildOrThrow();
+        product.changeTechnicalDescriptionTo(technicalDescription);
+        return this;
+    }
+
+    public ProductBuilder referenced(final Reference reference) {
+        buildOrThrow();
+        product.changeReferenceTo(reference);
+        return this;
+    }
+
+    public ProductBuilder productionCoded(final ProductionCode productionCode) {
+        buildOrThrow();
+        product.changeProductionCodeTo(productionCode);
+        return this;
+    }
+
+    public ProductBuilder costing(final Price price) {
+        buildOrThrow();
+        product.changePriceTo(price);
+        return this;
+    }
+
+    public ProductBuilder mesuring(final Mesurements mesurements) {
+        buildOrThrow();
+        product.changeMesurementsTo(mesurements);
+        return this;
+    }
+
+    public ProductBuilder located(final StorageArea storageArea) {
+        buildOrThrow();
+        product.changeStorageAreaTo(storageArea);
+        return this;
+    }
+
+
+    private Product buildOrThrow() {
+        if (product != null) {
+            return product;
+        } else if (category != null && internalCode != null && shortDescription != null && extendedDescription != null && barcode != null) {
+            product = new Product(category ,  internalCode,  shortDescription,
+                     extendedDescription,  technicalDeescription,
+                     brand,  reference,  productionCode,  barcode,  price);
+            return product;
+        } else {
+            throw new IllegalStateException();
+        }
+    }
+
+
+    public ProductBuilder withPhotos(final Set<byte[]> photos) {
+        // we will simply ignore if we receive a null set
+        if (photos != null) {
+            photos.forEach(this::withPhoto);
+        }
+        return this;
+    }
+
+
+    public ProductBuilder withPhoto(final byte[] photo) {
+        buildOrThrow();
+        product.addPhoto(photo);
+        return this;
+    }
+
+
+    @Override
+    public Product build() {
+        final Product ret = buildOrThrow();
+        // make sure we will create a new instance if someone reuses this builder and do not change
+        // the previously built dish.
+        product = null;
+        return ret;
+    }
+
 }
