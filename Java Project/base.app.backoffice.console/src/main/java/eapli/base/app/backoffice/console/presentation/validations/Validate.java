ff4a74d7af480f4420b43029f0728420c3f96c1a
Joao_Silva_1200616 <1200616@isep.ipp.pt>
2022-05-01 23:28:39 +0100

    #3 Minor Fix

--- Java Project/base.app.backoffice.console/src/main/java/eapli/base/app/backoffice/console/presentation/validations/Validate.java
+++ Java Project/base.app.backoffice.console/src/main/java/eapli/base/app/backoffice/console/presentation/validations/Validate.java
@@ -2,25 +2,27 @@ package eapli.base.app.backoffice.console.presentation.validations;
 
 import eapli.framework.io.util.Console;
 
+import java.util.Objects;
+
 public class Validate {
 
     public static String ValidateSort(String sort, int i) {
-        int x;
+        String x;
         while(i == 0){
-            x = Integer.parseInt(Console.readLine("Sort by (1.Internal Code , 2.Brand , 3.Category , 4.Unit Price :"));
-            if(x == 1) {
+            x = Console.readLine("Sort by (1.Internal Code , 2.Brand , 3.Category , 4.Unit Price :");
+            if(Objects.equals(x, "1")) {
                 sort = "p.products.internalCode";
                 i++;
             }
-            else if(x == 2){
+            else if(Objects.equals(x, "2")){
                 sort = "p.products.brand";
                 i++;
             }
-            else if(x == 3){
+            else if(Objects.equals(x, "3")){
                 sort = "p.products.productCategory";
                 i++;
             }
-            else if(x == 4){
+            else if(Objects.equals(x, "4")){
                 sort = "p.products.priceWithTaxes";
                 i++;
             }else{
