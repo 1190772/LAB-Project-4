d150a0b05fafad85be97c54b4ec3cd6f99abfe76
Joao_Silva_1200616 <1200616@isep.ipp.pt>
2022-05-17 16:01:01 +0100

    #8 product bootstrapper

---
+++ Java Project/base.bootstrappers/src/main/java/eapli/base/infrastructure/bootstrapers/masterdata/ProductBootstrapper.java
@@ -0,0 +1,67 @@
+package eapli.base.infrastructure.bootstrapers.masterdata;
+
+import eapli.base.application.product.SpecifyProductController;
+import eapli.base.domain.model.category.AlphaNumericCode;
+import eapli.base.domain.model.category.AlphaNumericCodeException;
+import eapli.base.domain.model.category.Category;
+import eapli.base.domain.model.product.Product;
+import eapli.base.domain.persistence.CategoryRepository;
+import eapli.framework.actions.Action;
+import eapli.framework.domain.repositories.ConcurrencyException;
+import eapli.framework.domain.repositories.IntegrityViolationException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ProductBootstrapper implements Action {
+
+    private static final Logger LOGGER = LogManager.getLogger(ProductBootstrapper.class);
+
+
+    SpecifyProductController controller = new SpecifyProductController();
+    CategoryRepository repo;
+
+
+    @Override
+    public boolean execute() {
+
+        try {
+            final Category cat1 = repo
+                    .ofIdentity(AlphaNumericCode.valueOf("123"))
+                    .orElseThrow(IllegalArgumentException::new);
+            final Category cat2 = repo
+                    .ofIdentity(AlphaNumericCode.valueOf("123"))
+                    .orElseThrow(IllegalArgumentException::new);
+            final Category cat3 = repo
+                    .ofIdentity(AlphaNumericCode.valueOf("321"))
+                    .orElseThrow(IllegalArgumentException::new);
+
+
+            register(cat1,"123456", "A Computer", "A Computer used for gaming", "Asus", 313243L, 22d, 33d, 4d, 3d, 999);
+
+            register(cat2,"134545", "A Computer", "A Computer used for working", "HP", 324364L, 25d, 30d, 3.6d, 2.7d, 899);
+
+            register(cat3,"134535", "A Laptop", "A Computer used for gaming", "Lenovo", 313543L, 23d, 32d, 3.9d, 3d, 1099);
+
+        } catch (AlphaNumericCodeException e) {
+            e.printStackTrace();
+        }
+
+        return true;
+    }
+
+    private void register(final Category productCategory, final String internalCode, final String shortDescription, final String extendedDescription,
+                          final String brand, final Long barcode, final Double width, final Double length,
+                          final Double height, final Double weight, final double price){
+
+        try{
+            System.out.println("3");
+            final Product p = controller.registerProduct(productCategory, internalCode, shortDescription,extendedDescription, null, brand, null, null, barcode, width, length, height, weight, price, null);
+            LOGGER.debug(p);
+        } catch (final IntegrityViolationException | ConcurrencyException e) {
+
+            LOGGER.error("this should not happen", e);
+        }
+
+    }
+
+}
