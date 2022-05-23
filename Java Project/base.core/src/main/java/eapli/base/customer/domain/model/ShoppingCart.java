642763a2fdfa80c5d1a65650963cf97489ab52eb
1190679 <1190679@isep.ipp.pt>
2022-05-23 20:14:38 +0100

    #14:US1501: Implementation of the ShoppingCart class

---
+++ Java Project/base.core/src/main/java/eapli/base/customer/domain/model/ShoppingCart.java
@@ -0,0 +1,43 @@
+package eapli.base.customer.domain.model;
+
+import eapli.base.product.domain.model.*;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class ShoppingCart {
+    @Id
+    private Long id;
+
+    @Embedded
+    ProductsList prodList;
+
+    @Embedded
+    PriceWithoutTaxes pwTt;
+
+    @Embedded
+    PriceWithTaxes pwt;
+
+    public ShoppingCart(){
+        this.prodList = new ProductsList();
+        pwTt=new PriceWithoutTaxes((double) 0);
+        pwt=new PriceWithTaxes((double) 0);
+    }
+
+    public ProductsList addProduct(Product prod){
+        prodList.addProduct(prod);
+        return prodList;
+    }
+
+    @Override
+    public String toString() {
+        return "ShoppingCart{" +
+                "id=" + id +
+                ", prodList=" + prodList +
+                ", pwTt=" + pwTt +
+                ", pwt=" + pwt +
+                '}';
+    }
+}
