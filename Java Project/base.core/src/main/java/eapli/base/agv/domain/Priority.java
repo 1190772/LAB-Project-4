2e2711baebbca4b85593a45d68cbc5e87e2ad721
1190384 <1190384@isep.ipp.pt>
2022-05-29 00:06:15 +0100

    #21 Priority class

---
+++ Java Project/base.core/src/main/java/eapli/base/agv/domain/Priority.java
@@ -0,0 +1,53 @@
+package eapli.base.agv.domain;
+
+import eapli.framework.domain.model.ValueObject;
+import eapli.framework.validations.Preconditions;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Priority implements ValueObject, Serializable, Comparable<Priority> {
+
+    private int priority;
+
+    public Priority(){}
+
+    public Priority(int priority) {
+        Preconditions.nonNegative(priority);
+        if(priority>5)
+            throw new IllegalArgumentException("Prioridade fora do limite");
+        this.priority = priority;
+    }
+
+    public static Priority valueOf(final int prioridade) {
+        return new Priority(prioridade);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Priority)) return false;
+        Priority that = (Priority) o;
+        return priority == that.priority;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(priority);
+    }
+
+    @Override
+    public String toString() {
+        return "Priority= " + priority;
+    }
+
+    @Override
+    public int compareTo(Priority o) {
+        if(this.priority==o.priority)
+            return 0;
+        else if(this.priority>o.priority){
+            return 1;
+        }
+        return -1;
+    }
+}
