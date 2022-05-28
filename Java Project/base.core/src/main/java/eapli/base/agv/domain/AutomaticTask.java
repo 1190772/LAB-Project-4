9135ebfba09ddad1029f4a9215b1396826dad5f2
1190384 <1190384@isep.ipp.pt>
2022-05-29 00:05:14 +0100

    #21 AutomaticTask class

---
+++ Java Project/base.core/src/main/java/eapli/base/agv/domain/AutomaticTask.java
@@ -0,0 +1,33 @@
+package eapli.base.agv.domain;
+
+import eapli.framework.domain.model.AggregateRoot;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class AutomaticTask extends Task implements AggregateRoot<Integer>, Serializable
+{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+
+    public AutomaticTask(){
+
+
+    }
+    public AutomaticTask(String description, Status approval, Priority priority){
+        super(description,approval,priority);
+    }
+
+    public int getId(){
+
+        return id;
+    }
+
+}
