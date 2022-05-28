ac4c74bb147f84856674709b3ba3e9eca0129ce2
1190384 <1190384@isep.ipp.pt>
2022-05-29 00:09:29 +0100

    #21 AutomaticTaskRepository class

---
+++ Java Project/base.core/src/main/java/eapli/base/agv/Repository/AutomaticTaskRepository.java
@@ -0,0 +1,7 @@
+package eapli.base.agv.Repository;
+
+import eapli.base.agv.domain.AutomaticTask;
+import eapli.framework.domain.repositories.DomainRepository;
+
+public interface AutomaticTaskRepository extends DomainRepository<Integer, AutomaticTask> {
+}
