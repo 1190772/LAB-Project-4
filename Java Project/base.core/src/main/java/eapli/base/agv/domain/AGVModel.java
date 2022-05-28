66201c1d70891b7c11499f928c0579d92d36404a
1190384 <1190384@isep.ipp.pt>
2022-05-29 00:04:31 +0100

    #21 AGV and AGVModel classes

---
+++ Java Project/base.core/src/main/java/eapli/base/agv/domain/AGVModel.java
@@ -0,0 +1,25 @@
+package eapli.base.agv.domain;
+
+import eapli.framework.domain.model.ValueObject;
+
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+
+@Embeddable
+public class AGVModel implements ValueObject, Serializable, Comparable<AGVModel> {
+
+    private String agvModel;
+
+    public AGVModel(String model){
+        this.agvModel=model;
+    }
+
+    protected AGVModel() {
+    }
+
+    @Override
+    public int compareTo(AGVModel o) {
+        return this.agvModel.compareTo(o.agvModel);
+    }
+
+}
