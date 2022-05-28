1db4991b699d25813b492407e7269c269079a339
1190384 <1190384@isep.ipp.pt>
2022-05-29 00:05:32 +0100

    #21 Info class

---
+++ Java Project/base.core/src/main/java/eapli/base/agv/domain/Info.java
@@ -0,0 +1,107 @@
+package eapli.base.agv.domain;
+
+import eapli.base.order.domain.model.Order;
+import eapli.framework.domain.model.AggregateRoot;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Date;
+
+@Entity
+public class Info implements AggregateRoot<Integer>, Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Temporal(TemporalType.DATE)
+    private Date dataLimite;
+    @Temporal(TemporalType.DATE)
+    private Date dataFim;
+    @Temporal(TemporalType.DATE)
+    private Date dataInicio;
+
+    private Status status;
+    private int tempoDecorrido;
+    private int prioridade; //prioridade
+
+    @OneToOne
+    private Order order;
+
+    @OneToOne
+    private AGV agv;
+
+    @OneToOne
+    private Task task;
+
+    public Info(){}
+
+    public Info(Date dataLimite, int prioridade){
+        this.dataLimite=dataLimite;
+        this.prioridade=prioridade;
+        this.status = Status.NOT_STARTED;
+        this.dataInicio = Calendar.getInstance().getTime();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Date getDataLimite() {
+        return dataLimite;
+    }
+
+    public Date getDataFim() {
+        return dataFim;
+    }
+
+    public Date getDataInicio() {
+        return dataInicio;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public int getTempoDecorrido() {
+        return tempoDecorrido;
+    }
+
+    public int getPrioridade() {
+        return prioridade;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public AGV getAgv() {
+        return agv;
+    }
+
+    public Task getTask() {
+        return task;
+    }
+
+    public void changeStatus(Status estado) {
+        this.status = estado;
+    }
+
+
+
+
+    @Override
+    public boolean sameAs(Object other) {
+        return false;
+    }
+
+    @Override
+    public Integer identity() {
+        return null;
+    }
+
+    public void associateAGV(AGV agv) {
+        this.agv = agv;
+    }
+}
