359bc4a55fae6362578897e5c06fdb071408a60d
1190384 <1190384@isep.ipp.pt>
2022-05-29 00:09:47 +0100

    #21 InfoRepository class

---
+++ Java Project/base.core/src/main/java/eapli/base/agv/Repository/InfoRepository.java
@@ -0,0 +1,7 @@
+package eapli.base.agv.Repository;
+
+import eapli.base.agv.domain.Info;
+import eapli.framework.domain.repositories.DomainRepository;
+
+public interface InfoRepository extends DomainRepository<Integer, Info> {
+}
