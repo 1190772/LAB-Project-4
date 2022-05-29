e478f5ee26545d5dc545d62f6eea81125b1a5ce7
1190772 <1190772@isep.ipp.pt>
2022-05-29 22:15:58 +0100

    #6 Comment Classes that are with errors

--- Java Project/base.core/src/main/java/eapli/base/importwarehouse/application/WarehouseJsonReader.java
+++ Java Project/base.core/src/main/java/eapli/base/importwarehouse/application/WarehouseJsonReader.java
@@ -1,20 +1,20 @@
-package eapli.base.importwarehouse.application;
-
-import eapli.base.importwarehouse.domain.JsonReader;
-import eapli.base.importwarehouse.domain.WarehousePlant;
-
-public class WarehouseJsonReader implements JsonReader<WarehousePlant> {
-
-
-
-    @Override
-    public WarehousePlant parse(String filePath){
-
-
-
-
-    }
-
-
-
-}
+//package eapli.base.importwarehouse.application;
+//
+//import eapli.base.importwarehouse.domain.JsonReader;
+//import eapli.base.importwarehouse.domain.WarehousePlant;
+//
+//public class WarehouseJsonReader implements JsonReader<WarehousePlant> {
+//
+//
+//
+//    @Override
+//    public WarehousePlant parse(String filePath){
+//
+//
+//
+//
+//    }
+//
+//
+//
+//}
