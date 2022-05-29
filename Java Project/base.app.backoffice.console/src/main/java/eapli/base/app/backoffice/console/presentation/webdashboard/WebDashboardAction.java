dc536540669b089c550286b850e8f67ce6909e06
Joao_Silva_1200616 <1200616@isep.ipp.pt>
2022-05-29 22:24:53 +0100

    #17 WebDashboardActionI update

--- Java Project/base.app.backoffice.console/src/main/java/eapli/base/app/backoffice/console/presentation/webdashboard/WebDashboardAction.java
+++ Java Project/base.app.backoffice.console/src/main/java/eapli/base/app/backoffice/console/presentation/webdashboard/WebDashboardAction.java
@@ -4,13 +4,14 @@ import eapli.base.app.backoffice.console.presentation.productCatalog.ListProduct
 import eapli.framework.actions.Action;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 public class WebDashboardAction implements Action {
     @Override
     public boolean execute() {
         try {
             return new WebDashboardUI().show();
-        } catch (IOException e) {
+        } catch (IOException | URISyntaxException e) {
             e.printStackTrace();
         }
         return false;
