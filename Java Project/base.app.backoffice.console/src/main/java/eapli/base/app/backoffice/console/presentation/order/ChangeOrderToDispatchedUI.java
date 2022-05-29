795bb081f64c3a412f563df5867e28571ff19ea3
1190772 <1190772@isep.ipp.pt>
2022-05-29 20:21:01 +0100

    #20 OrderStatusController

---
+++ Java Project/base.app.backoffice.console/src/main/java/eapli/base/app/backoffice/console/presentation/order/ChangeOrderToDispatchedUI.java
@@ -0,0 +1,16 @@
+package eapli.base.app.backoffice.console.presentation.order;
+
+import eapli.framework.presentation.console.AbstractUI;
+
+public class ChangeOrderToDispatchedUI extends AbstractUI {
+
+    @Override
+    protected boolean doShow() {
+        return false;
+    }
+
+    @Override
+    public String headline() {
+        return null;
+    }
+}
---
+++ Java Project/base.core/src/main/java/eapli/base/order/application/OrderStatusController.java
@@ -0,0 +1,35 @@
+package eapli.base.order.application;
+
+import eapli.base.infrastructure.persistence.PersistenceContext;
+import eapli.base.order.domain.model.Order;
+import eapli.base.order.domain.model.OrderStatus;
+import eapli.base.order.domain.persistence.OrderRepository;
+import eapli.base.usermanagement.domain.BaseRoles;
+import eapli.framework.infrastructure.authz.application.AuthorizationService;
+import eapli.framework.infrastructure.authz.application.AuthzRegistry;
+
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+public class OrderStatusController {
+    private final OrderRepository orderRepository = PersistenceContext.repositories().order();
+    private final AuthorizationService authz = AuthzRegistry.authorizationService();
+
+    public Iterable<Order> ordersPrepared(){
+        authz.ensureAuthenticatedUserHasAnyOf(BaseRoles.WAREHOUSE_EMPLOYEE);
+        final Iterable<Order> orders = orderRepository.readyOrders();
+        return StreamSupport.stream(orders.spliterator(),true).collect(Collectors.toUnmodifiableList());
+    }
+
+    public void changeOrderToDispatched (Long id){
+        authz.ensureAuthenticatedUserHasAnyOf(BaseRoles.WAREHOUSE_EMPLOYEE);
+        Order order = (Order) orderRepository.findOrderById(id);
+        if(order != null){
+            order.changeStatus(OrderStatus.DELIVERED);
+            orderRepository.save(order);
+
+        }else{
+            System.out.println("Invalid ID.");
+        }
+    }
+}
