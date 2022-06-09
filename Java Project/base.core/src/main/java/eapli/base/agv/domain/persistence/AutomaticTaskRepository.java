60b2351a208406ab9671c2cfc6a6b1023c7a84e0
1190679 <1190679@isep.ipp.pt>
2022-06-09 15:18:57 +0100

    #9: US2002 - Repositioning of some classes existent in the project

--- Java Project/base.core/src/main/java/eapli/base/agv/domain/persistence/AutomaticTaskRepository.java
+++ Java Project/base.core/src/main/java/eapli/base/agv/domain/persistence/AutomaticTaskRepository.java
@@ -1,4 +1,4 @@
-package eapli.base.agv.Repository;
+package eapli.base.agv.domain.persistence;
 
 import eapli.base.agv.domain.model.AutomaticTask;
 import eapli.framework.domain.repositories.DomainRepository;
