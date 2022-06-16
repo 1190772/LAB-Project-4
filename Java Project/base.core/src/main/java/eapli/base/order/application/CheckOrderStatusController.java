e9c6037681d1481162a5c2804e0a63808f1dd856
1190772 <1190772@isep.ipp.pt>
2022-06-16 19:10:05 +0100

    #27 CheckOrderStatusController

--- Java Project/base.core/src/main/java/eapli/base/order/application/CheckOrderStatusController.java
+++ Java Project/base.core/src/main/java/eapli/base/order/application/CheckOrderStatusController.java
@@ -1,6 +1,26 @@
 package eapli.base.order.application;
 
+import eapli.base.infrastructure.persistence.PersistenceContext;
+import eapli.base.order.domain.model.Order;
+import eapli.base.order.repositories.OrderRepository;
+import eapli.base.usermanagement.domain.BaseRoles;
+import eapli.framework.infrastructure.authz.application.AuthorizationService;
+import eapli.framework.infrastructure.authz.application.AuthzRegistry;
+
+import java.util.List;
+
 public class CheckOrderStatusController {
+
+    private final OrderRepository orderRepository = PersistenceContext.repositories().order();
+    private final AuthorizationService authz = AuthzRegistry.authorizationService();
+
+    public List<Order> checkOrderStatus(long id){
+        authz.ensureAuthenticatedUserHasAnyOf(BaseRoles.CUSTOMER);
+        while(orderRepository.containsOfIdentity(id)){
+            System.out.println(orderRepository);
+        }
+
+    }
 
 
 }
