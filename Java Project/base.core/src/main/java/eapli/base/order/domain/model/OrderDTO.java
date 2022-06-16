eee92f9af44079b6bcaf7f0d89593846c0f1c11d
1190772 <1190772@isep.ipp.pt>
2022-06-16 21:21:40 +0100

    #27 OrderDTO

--- Java Project/base.core/src/main/java/eapli/base/order/domain/model/OrderDTO.java
+++ Java Project/base.core/src/main/java/eapli/base/order/domain/model/OrderDTO.java
@@ -1,5 +1,6 @@
 package eapli.base.order.domain.model;
 
+import eapli.base.customer.domain.model.CustomerDTO;
 import eapli.base.product.domain.model.OrderedProduct;
 import eapli.base.product.domain.model.OrderedProductDTO;
 
@@ -18,4 +19,18 @@ public class OrderDTO {
     public OrderStatus orderStatus;
     public double weight;
     public CustomerDTO customerDTO;
+
+    public OrderDTO(long id, List<OrderedProductDTO> listOfOrders, double priceWithoutTaxes, double priceWithTaxes,
+                    Date creationDate, String email, String address, OrderStatus orderStatus, double weight, CustomerDTO customerDTO) {
+        this.id = id;
+        this.listOfOrders = listOfOrders;
+        this.priceWithoutTaxes = priceWithoutTaxes;
+        this.priceWithTaxes = priceWithTaxes;
+        this.creationDate = creationDate;
+        this.email = email;
+        this.address = address;
+        this.orderStatus = orderStatus;
+        this.weight = weight;
+        this.customerDTO = customerDTO;
+    }
 }
