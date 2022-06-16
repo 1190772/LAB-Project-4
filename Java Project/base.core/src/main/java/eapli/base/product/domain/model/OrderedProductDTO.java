13a5abbe7ae58a83492803bfc7673db3923b0c80
1190772 <1190772@isep.ipp.pt>
2022-06-16 21:15:42 +0100

    #27 OrderedProductDTO

--- Java Project/base.core/src/main/java/eapli/base/product/domain/model/OrderedProductDTO.java
+++ Java Project/base.core/src/main/java/eapli/base/product/domain/model/OrderedProductDTO.java
@@ -1,4 +1,12 @@
 package eapli.base.product.domain.model;
 
 public class OrderedProductDTO {
+
+    public ProductDTO productDTO;
+    public int quantity;
+
+    public OrderedProductDTO(ProductDTO productDTO, int quantity) {
+        this.productDTO = productDTO;
+        this.quantity = quantity;
+    }
 }
