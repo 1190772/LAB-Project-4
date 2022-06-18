1ab9606c696554550061231e648ead74e3c1ba39
1190384 <1190384@isep.ipp.pt>
2022-06-18 22:40:15 +0100

    #21 Task class updated

--- Java Project/base.core/src/main/java/eapli/base/agv/domain/model/Task.java
+++ Java Project/base.core/src/main/java/eapli/base/agv/domain/model/Task.java
@@ -1,58 +1,75 @@
 package eapli.base.agv.domain.model;
 
 
+import eapli.base.order.domain.model.Order;
+import eapli.base.product.domain.model.Product;
 import eapli.framework.domain.model.AggregateRoot;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Entity
-public class Task implements AggregateRoot<Integer>, Serializable {
+public class Task implements AggregateRoot<String>, Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    //  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private String id;
 
     private Priority priority;
 
+    private String info;
+
+    private boolean approved;
     private Status status;
 
-    private String info;
-    private boolean approved;
+    @OneToOne(optional = false, cascade = CascadeType.ALL)
+    private Order order;
 
+    @OneToOne(optional = false, cascade = CascadeType.ALL)
+    private AGV agv;
 
     public Task() {
     }
 
-    public Task(String info, Status status , Priority priority) {
-        this.info = info;
-        this.status = status;
+
+    public Task(String id, AGV agv, Order order, Status status, Priority priority) {
+        this.id = id;
+        this.agv = agv;
+        this.order = order;
+        this.status = Status.ASSOCIATED;
         this.priority = priority;
     }
 
 
     @Override
     public boolean sameAs(Object other) {
-        return false;
+        if (!(other instanceof Product)) {
+            return false;
+        }
+        final Task that = (Task) other;
+        if (this == that) {
+            return true;
+        }
+        return identity().equals(that.identity());
     }
 
     @Override
-    public Integer identity() {
+    public String identity() {
         return id;
     }
 
-    public Priority getPriority() { return priority;}
+    public Priority getPriority() {
+        return priority;
+    }
 
-    public Status getStatus() { return status;}
+    public Status getStatus() {
+        return status;
+    }
 
-    public String getInfo() { return info;}
-    public boolean getType(){return this.approved;}
 
-    public int getId() {
-        return id;
+    public String getId() {
+        return this.id;
     }
 
     @Override
@@ -63,4 +80,38 @@ public class Task implements AggregateRoot<Integer>, Serializable {
         return id == tarefa.id;
     }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    public AGV getAgv() {
+        return agv;
+    }
+
+    public void setAgv(AGV agv) {
+        this.agv = agv;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    /**
+     * Changes the task status.
+     */
+    public void changeStatus(Status status) {
+        this.status = status;
+    }
+
+    public String getInfo() { return info;
+    }
+
+    public boolean getType() {
+        return this.approved;
+    }
 }
