12053d7abdf05f6932149cc6bf6a1539dfa51aef
1190679 <1190679@isep.ipp.pt>
2022-06-19 18:35:39 +0100

    #29: US5100 - Mapping the AGVs

---
+++ Java Project/movementagvmanagement/src/main/java/domain/Communication.java
@@ -0,0 +1,222 @@
+package domain;
+
+import java.util.LinkedList;
+
+public class Communication {
+
+    //Sensores
+    private int sensor_norte;
+
+    private int sensor_nordeste;
+
+    private int sensor_este;
+
+    private int sensor_sudeste;
+
+    private int sensor_sul;
+
+    private int sensor_sudoeste;
+
+    private int sensor_oeste;
+
+    private int sensor_noroeste;
+
+    //Direccao pode ser esquerda, direita, cima, baixo
+    private String direccao;
+
+
+    //Velocidade
+    private Double velocity;
+
+
+    //Bateria
+    private final Double MAX_BATTERY;
+
+    private Double actualBattery;
+
+
+    //Posicoes
+    private final Location DOCK_LOCATION;
+
+    private LinkedList<Location> productLocations;
+
+    private Location actualPosition;
+
+    private LinkedList<Location> route;
+
+    //Construtor
+    public Communication(int sensor_norte, int sensor_nordeste, int sensor_este, int sensor_sudeste, int sensor_sul, int sensor_sudoeste,
+                         int sensor_oeste, int sensor_noroeste, Double battery, Location dock, LinkedList<Location> productLocations) {
+        this.sensor_norte = sensor_norte;
+        this.sensor_nordeste = sensor_nordeste;
+        this.sensor_este = sensor_este;
+        this.sensor_sudeste = sensor_sudeste;
+        this.sensor_sul = sensor_sul;
+        this.sensor_sudoeste = sensor_sudoeste;
+        this.sensor_oeste = sensor_oeste;
+        this.sensor_noroeste = sensor_noroeste;
+        this.MAX_BATTERY = battery;
+        this.DOCK_LOCATION = dock;
+        this.productLocations = productLocations;
+        actualBattery = MAX_BATTERY;
+        direccao = "";
+        velocity = 0.0;
+        actualPosition = DOCK_LOCATION;
+        route = new LinkedList<>();
+    }
+
+
+    //Setters dos sensores
+    public void setSensor_norte(int sensor_norte) {
+        this.sensor_norte = sensor_norte;
+    }
+
+    public void setSensor_nordeste(int sensor_nordeste) {
+        this.sensor_nordeste = sensor_nordeste;
+    }
+
+    public void setSensor_este(int sensor_este) {
+        this.sensor_este = sensor_este;
+    }
+
+    public void setSensor_sudeste(int sensor_sudeste) {
+        this.sensor_sudeste = sensor_sudeste;
+    }
+
+    public void setSensor_sul(int sensor_sul) {
+        this.sensor_sul = sensor_sul;
+    }
+
+    public void setSensor_sudoeste(int sensor_sudoeste) {
+        this.sensor_sudoeste = sensor_sudoeste;
+    }
+
+    public void setSensor_oeste(int sensor_oeste) {
+        this.sensor_oeste = sensor_oeste;
+    }
+
+    public void setSensor_noroeste(int sensor_noroeste) {
+        this.sensor_noroeste = sensor_noroeste;
+    }
+
+
+    //Getters dos Sensores
+    public int getSensor_norte() {
+        return sensor_norte;
+    }
+
+    public int getSensor_nordeste() {
+        return sensor_nordeste;
+    }
+
+    public int getSensor_este() {
+        return sensor_este;
+    }
+
+    public int getSensor_sudeste() {
+        return sensor_sudeste;
+    }
+
+    public int getSensor_sul() {
+        return sensor_sul;
+    }
+
+    public int getSensor_sudoeste() {
+        return sensor_sudoeste;
+    }
+
+    public int getSensor_oeste() {
+        return sensor_oeste;
+    }
+
+    public int getSensor_noroeste() {
+        return sensor_noroeste;
+    }
+
+
+    //Direcao
+    public String getDireccao() {
+        return direccao;
+    }
+
+    public void setDireccao(String direccao) {
+        this.direccao = direccao;
+    }
+
+    //Velocidade
+    public Double getVelocity() {
+        return velocity;
+    }
+
+    public void setVelocity(Double velocity) {
+        this.velocity = velocity;
+    }
+
+
+    //Batteria actual
+    public Double getMAX_BATTERY() {
+        return MAX_BATTERY;
+    }
+
+    public Double getActualBattery() {
+        return actualBattery;
+    }
+
+    public void setActualBattery(Double actualBattery) {
+        this.actualBattery = actualBattery;
+    }
+
+
+    //Actualiza positcao actual
+    public Location getActualPosition() {
+        return actualPosition;
+    }
+
+    public void setActualPosition(Location actualPosition) {
+        this.actualPosition = actualPosition;
+    }
+
+
+    //Quando encontra produto remove da lista
+    public LinkedList<Location> getProductLocations() {
+        return productLocations;
+    }
+
+    //Prepara nova ordem de produtos
+    public void setProductLocations(LinkedList<Location> productLocations) {
+        this.productLocations = productLocations;
+    }
+
+
+    //Devolve rota para se mover
+    public LinkedList<Location> getRoute() {
+        return route;
+    }
+
+    //Recebe rota
+    public void setRoute(LinkedList<Location> route) {
+        this.route = route;
+    }
+
+    @Override
+    public String toString() {
+        return "Communication{" +
+                "sensor_norte=" + sensor_norte +
+                ", sensor_nordeste=" + sensor_nordeste +
+                ", sensor_este=" + sensor_este +
+                ", sensor_sudeste=" + sensor_sudeste +
+                ", sensor_sul=" + sensor_sul +
+                ", sensor_sudoeste=" + sensor_sudoeste +
+                ", sensor_oeste=" + sensor_oeste +
+                ", sensor_noroeste=" + sensor_noroeste +
+                ", direccao='" + direccao + '\'' +
+                ", velocity=" + velocity +
+                ", MAX_BATTERY=" + MAX_BATTERY +
+                ", actualBattery=" + actualBattery +
+                ", DOCK_LOCATION=" + DOCK_LOCATION +
+                ", productLocations=" + productLocations +
+                ", actualPosition=" + actualPosition +
+                ", route=" + route +
+                '}';
+    }
+}
---
+++ Java Project/movementagvmanagement/src/main/java/domain/Map.java
@@ -0,0 +1,240 @@
+package domain;
+
+import domain.graph.Algorithms;
+import domain.graph.map.MapGraph;
+import eapli.base.importwarehouse.domain.WarehousePlant;
+import eapli.base.importwarehouse.domain.aisles.Aisle;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.BinaryOperator;
+
+public class Map {
+
+    private List<Location> map;
+
+    public void createMap() {
+
+        MapGraph<Location, Integer> graph = new MapGraph<>(false);
+        WarehousePlant warehousePlant=null;
+
+        int nX = (int)warehousePlant.getWidth().width();
+        int nY = (int)warehousePlant.getLength().length();
+
+
+        System.out.println(nX);
+        System.out.println(nY);
+
+
+
+        map = new ArrayList<>();
+
+
+        for(int i = 1; i <= nX; i++){
+            for (int j = 1; j <= nY; j++) {
+                Location local = new Location(i, j, 0);
+                map.add(local);
+                graph.addVertex(local);
+            }
+        }
+
+
+        int auxXr;
+        int auxXl;
+        int auxYr;
+        int auxYl;
+        for(int i = 1; i <= nX; i++){
+            auxXr = i + 1;
+            auxXl = i - 1;
+            for (int j = 1; j <= nY; j++) {
+                auxYr = j + 1;
+                auxYl = j - 1;
+
+                if(auxXr >= 1 && auxXr <= nX) {
+                    graph.addEdge(graph.getVertex(i,j),graph.getVertex(auxXr,j),1);
+                }
+
+                if(auxYr >= 1 && auxYr <= nY) {
+                    graph.addEdge(graph.getVertex(i,j),graph.getVertex(i,auxYr),1);
+                }
+
+                if(auxXl >= 1 && auxXl <= nX) {
+                    graph.addEdge(graph.getVertex(i,j),graph.getVertex(auxXl,j),1);
+                }
+
+                if(auxYl >= 1 && auxYl <= nY) {
+                    graph.addEdge(graph.getVertex(i,j),graph.getVertex(i,auxYl),1);
+                }
+
+            }
+        }
+
+        List<Aisle> aisles = warehousePlant.getAisles();
+
+
+
+        for (Aisle al : aisles) {
+            int xBegin = Math.toIntExact(al.getBegin().getwSquare());
+            int yBegin = Math.toIntExact(al.getBegin().getlSquare());
+            int xEnd = Math.toIntExact(al.getEnd().getwSquare());
+            int yEnd = Math.toIntExact(al.getEnd().getlSquare());
+            int xDepth = Math.toIntExact(al.getDepth().getwSquare());
+            int yDepth = Math.toIntExact(al.getDepth().getwSquare());
+
+            System.out.println();
+
+            if(xBegin == xEnd){
+                if(xBegin <= xDepth) {
+                    for (int i = xBegin; i <= xDepth; i++) {
+                        for (int j = yBegin; j <= yEnd; j++) {
+                            graph.removeVertex(graph.getVertex(i,j));
+                        }
+                    }
+                } else {
+                    for (int i = xBegin; i >= xDepth; i--) {
+                        for (int j = yBegin; j <= yEnd; j++) {
+                            graph.removeVertex(graph.getVertex(i,j));
+                        }
+                    }
+                }
+            }
+            if(yBegin == yEnd) {
+                if(yBegin <= yDepth) {
+                    for (int i = yBegin; i <= yDepth; i++) {
+                        for (int j = xBegin; j <= xEnd; j++) {
+                            graph.removeVertex(graph.getVertex(j,i));
+                        }
+                    }
+                } else {
+                    for (int i = yBegin; i >= yDepth; i--) {
+                        for (int j = xBegin; j <= xEnd; j++) {
+                            graph.removeVertex(graph.getVertex(j,i));
+                        }
+                    }
+                }
+            }
+        }
+
+
+        Comparator<Integer> compint = new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return (o1 - o2);
+            }
+        };
+
+        BinaryOperator<Integer> summ = new BinaryOperator<Integer>() {
+            @Override
+            public Integer apply(Integer aDouble, Integer aDouble2) {
+                return aDouble + aDouble2;
+            }
+        };
+
+        LinkedList<Location> shortPath = new LinkedList<>();
+        Algorithms.shortestPath(graph, graph.getVertex(3,1),graph.getVertex(12,14), compint, summ,0,shortPath);
+
+
+        System.out.println(shortPath);
+
+    }
+
+    public void addDockPosition(Location location) {
+        for (Location l : map) {
+            if (l.getX() == location.getX() && l.getY() == location.getY()) {
+                l.setObstacle(1);
+
+            }
+        }
+    }
+
+    public void addShelfPosition(Location location) {
+        for (Location l : map) {
+            if (l.getX() == location.getX() && l.getY() == location.getY()) {
+                l.setObstacle(2);
+
+            }
+        }
+    }
+
+
+    public void changeAgvPosition(Location previousLocation, Location newLocation) {
+        for (Location l : map) {
+            if (l.getX() == previousLocation.getX() && l.getY() == previousLocation.getY()) {
+                l.setObstacle(previousLocation.getObstacle());
+            }
+            if (l.getX() == newLocation.getX() && l.getY() == newLocation.getY()) {
+                l.setObstacle(3);
+            }
+        }
+
+    }
+
+
+    public Location returnDownLocation(Location location) {
+        Location location1 = null;
+        for (Location l : map) {
+            if (l.getX() == location.getX() - 1 && l.getY() == location.getY()) {
+                location1 = l;
+            }
+        }
+        return location1;
+    }
+
+    public Location returnUpLocation(Location location) {
+        Location location1 = null;
+        for (Location l : map) {
+            if (l.getX() == location.getX() + 1 && l.getY() == location.getY()) {
+                location1 = l;
+            }
+        }
+        return location1;
+    }
+
+    public Location returnRightLocation(Location location) {
+        Location location1 = null;
+        for (Location l : map) {
+            if (l.getX() == location.getX() && l.getY() == location.getY() + 1) {
+                location1 = l;
+            }
+        }
+        return location1;
+    }
+
+    public Location returnLeftLocation(Location location) {
+        Location location1 = null;
+        for (Location l : map) {
+            if (l.getX() == location.getX() && l.getY() == location.getY() - 1) {
+                location1 = l;
+            }
+        }
+        return location1;
+    }
+
+    public void printMap() {
+
+        Location loc = map.get(map.size() - 1);
+        int intial = 0;
+        System.out.println("\n   | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |10 |11 |12 |13 |14 |15 |16 |17 |18 |19 |20 |");
+        for (int l = 0; l < loc.getX(); l++) {
+            String linha = "";
+            if (l < 9) {
+                linha = linha.concat(String.valueOf(l + 1)) + "  | ";
+            } else {
+                linha = linha.concat(String.valueOf(l + 1)) + " | ";
+            }
+            int fin = loc.getY() + intial;
+            for (int k = intial; k < fin; k++) {
+                linha = linha.concat(map.get(k).getObstacle() + " | ");
+                intial++;
+            }
+            System.out.println(linha);
+        }
+        System.out.println("\n0 -> No obstacles");
+        System.out.println("1 -> AGV Dock");
+        System.out.println("2 -> Shelf");
+        System.out.println("3 -> AGV");
+    }
+
+}
