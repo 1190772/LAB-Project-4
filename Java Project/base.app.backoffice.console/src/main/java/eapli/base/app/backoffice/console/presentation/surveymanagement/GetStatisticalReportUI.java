149a7e86e3bbe0c278c41031c870b93788bff468
Joao_Silva_1200616 <1200616@isep.ipp.pt>
2022-06-19 17:29:39 +0100

    #31 GetStatisticalReportUI

---
+++ Java Project/base.app.backoffice.console/src/main/java/eapli/base/app/backoffice/console/presentation/surveymanagement/GetStatisticalReportUI.java
@@ -0,0 +1,19 @@
+package eapli.base.app.backoffice.console.presentation.surveymanagement;
+
+import eapli.framework.presentation.console.AbstractUI;
+
+public class GetStatisticalReportUI extends AbstractUI {
+
+    @Override
+    protected boolean doShow() {
+
+        System.out.println("Not implemented yet");
+
+        return true;
+    }
+
+    @Override
+    public String headline() {
+        return null;
+    }
+}
